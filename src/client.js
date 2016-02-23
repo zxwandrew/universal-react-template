2c2121154b21bc04404cade049d239040e505786
Andrew Wang <zxwandrew@gmail.com>
2016-02-22 21:52:12 -0800

    browserhistory change

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import { routes } from './app/routes/routes';
 // import CoreLayout from "./layout/CoreLayout";
 // import HomeView from "./pages/HomeView";
@@ -9,5 +9,5 @@ import { routes } from './app/routes/routes';
 
 const app = document.getElementById('app');
 ReactDOM.render(
-  <Router history={hashHistory} routes={routes}></Router>,
+  <Router history={browserHistory} routes={routes}></Router>,
 app);
